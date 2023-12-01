b8d0b12b7c694216f1bcae741a04f0799c10485e
Joash Tubaga <joashtubaga@gmail.com>
2023-12-02 03:06:06 +0800

    Add resource file for module 14: The Browse Screen

---
+++ 14. The Browse Screen/Project/Challenge 10 Rewards/Challenge 10 Rewards/Resources/ColorExtensions.swift
@@ -0,0 +1,15 @@
+//  Copyright © 2020 Mark Moeykens. All rights reserved. | @BigMtnStudio
+
+import SwiftUI
+
+extension Color {
+    static let themeAccent = Color("AccentColor")
+    static let themeAccent2 = Color("AccentColor2")
+    static let themeAccentShadow = Color("AccentShadowColor")
+    static let themeBackground = Color("BackgroundColor")
+    static let themeBackground2 = Color("BackgroundColor2")
+    static let themeForeground = Color("ForegroundColor")
+    static let themeSecondary = Color("SecondaryColor")
+    static let themeSecondary2 = Color("SecondaryColor2")
+    static let themeTertiary = Color("TertiaryColor")
+}
